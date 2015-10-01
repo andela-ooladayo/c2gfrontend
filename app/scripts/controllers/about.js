339a0c28a285689aee475e320e6e55d49a9458a2
routerfrank <ayfrank2@hotmail.com>
2015-10-01 01:38:48 +0100

    initialize

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fgscApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fgscApp
+ */
+angular.module('fgscApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fgscApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fgscApp
+ */
+angular.module('fgscApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
